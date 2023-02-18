c638496558130d0f76e9d61168353881e216e0eb
Ligiano Azzi <lazzi@active.directory.farmers.journal>
2023-02-18 17:40:24 +0000

    trying to run cypress 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write('Yo yo yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
 
