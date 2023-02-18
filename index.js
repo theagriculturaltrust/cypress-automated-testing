d31a0af28d37faf25469ffad3907264c21586ec3
Ligiano Azzi <lazzi@active.directory.farmers.journal>
2023-02-18 17:38:08 +0000

    trying to run cypress 3

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const { exec } = require("child_process");
 exec("npx cypress run --key 7bed1257-f64f-4ebc-8c57-d6300d1b686f --record", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
+        alert("A")
         return;
     }
     if (stderr) {
         console.log(`stderr: ${stderr}`);
+        alert("B")
         return;
     }
     console.log(`stdout: ${stdout}`);
+    alert("C")
 });
