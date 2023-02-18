2c6fd1c5e3af8c35ac54bb9b7ee6afae67cbb5d2
Ligiano Azzi <lazzi@active.directory.farmers.journal>
2023-02-18 17:42:41 +0000

    trying to run cypress 5

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo yo yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
 
-const { exec } = require("child_process");
 
 exec("npx cypress run --key 7bed1257-f64f-4ebc-8c57-d6300d1b686f --record", (error, stdout, stderr) => {
     if (error) {
