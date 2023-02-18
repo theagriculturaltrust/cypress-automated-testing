37cb89cb535ea084d00f26a5d313c1f19b87c77f
Ligiano Azzi <lazzi@active.directory.farmers.journal>
2023-02-18 19:06:51 +0000

    trying to run cypress 14

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/*var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo! Yo! Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);*/
 alert("test")
 const { exec } = require('node:child_process')
 
@@ -18,3 +12,11 @@ exec('cypress run --key 7bed1257-f64f-4ebc-8c57-d6300d1b686f', (err, output) => 
     // log the output received from the command
     console.log("Output: \n", output)
 })
+
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`)
+    res.write('Yo! Yo! Yo!');
+    res.end();
+}).listen(process.env.PORT || 3000);
+
