df7b48b212f47045fe01ca45d4b5646a8084ea01
Ligiano Azzi <lazzi@active.directory.farmers.journal>
2023-02-18 17:44:58 +0000

    trying to run cypress 7

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-
-const { exec } = require("child_process");
-
-exec("npx cypress run --key 7bed1257-f64f-4ebc-8c57-d6300d1b686f --record", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        alert("A")
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        alert("B")
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    alert("C")
-});
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`)
+    res.write('Yo yo yo!');
+    res.end();
+}).listen(process.env.PORT || 3000);
