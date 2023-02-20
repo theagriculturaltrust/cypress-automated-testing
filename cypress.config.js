5c711cf3dca3b5aab4441b2afb1681d8b8af8b5a
Ligiano Azzi <lazzi@active.directory.farmers.journal>
2023-02-20 18:53:25 +0000

    trying to config github actions

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
       // e2e testing node events setup code
       .run({
          // the path is relative to the current working directory
-         spec: './cypress/e2e/1-geting-started/subscribe-page.cy',
+          './cypress/e2e/1-geting-started/subscribe-page.cy',
        })
     },
   },
