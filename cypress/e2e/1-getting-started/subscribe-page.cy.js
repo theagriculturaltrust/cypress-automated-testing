8bc004f9bb0f5d72cbd0183363bad7122ebf4ff6
theagriculturaltrust <126018554+theagriculturaltrust@users.noreply.github.com>
2023-11-09 17:32:50 +0000

    Update subscribe-page.cy.js

--- cypress/e2e/1-getting-started/subscribe-page.cy.js
+++ cypress/e2e/1-getting-started/subscribe-page.cy.js
@@ -79,11 +79,15 @@ it('checking SUBSCRIBE SUCCESS - IF STATUS 200', () => {
 
 
 it('GARDEN.IE checking subscribe.php -- PLANS -- component', () => {
-  cy.visit('https://subscribe.garden.ie/', { timeout: 10000 });
+  cy.visit('https://subscribe.garden.ie/', { timeout: 1000 });
   cy.get('#slot-digital-standard').should('include.text', 'Get it delivered');
   cy.get('#slot-complete').should('include.text', 'Buy as a gift');
-
+  cy.get('.subs-plan-box-content').should(($element) => {
+    const backgroundColor = $element.css('background-color');
+    expect(backgroundColor).to.equal('#fff'); // Use the RGB format directly
+  });
 });
+
 
 
 
