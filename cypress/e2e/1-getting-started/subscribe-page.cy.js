3af55741ad7aa1582218490d20e98d087434320c
theagriculturaltrust <126018554+theagriculturaltrust@users.noreply.github.com>
2023-11-09 16:53:10 +0000

    Update subscribe-page.cy.js

--- cypress/e2e/1-getting-started/subscribe-page.cy.js
+++ cypress/e2e/1-getting-started/subscribe-page.cy.js
@@ -78,16 +78,15 @@ it('checking SUBSCRIBE SUCCESS - IF STATUS 200', () => {
 });
 
 
-  it('GARDEN.IE checking subscribe.php -- PLANS -- component ', () => {
-    cy.visit('https://subscribe.garden.ie/', { timeout: 1000 })
-    cy.get('#slot-digital-standard').should('include.text', 'Get it delivered')
-    cy.get('#slot-complete').should('include.text', 'Buy as a gift')
-    cy.get('.subs-plan-box-content')
-    .should(($element) => {
-      const backgroundColor = $element.css('background-color');
-      expect(backgroundColor).to.equal('#fff'); // Use the hex color directly
-    })
+it('GARDEN.IE checking subscribe.php -- PLANS -- component', () => {
+  cy.visit('https://subscribe.garden.ie/', { timeout: 1000 });
+  cy.get('#slot-digital-standard').should('include.text', 'Get it delivered');
+  cy.get('#slot-complete').should('include.text', 'Buy as a gift');
+  cy.get('.subs-plan-box-content').should(($element) => {
+    const backgroundColor = $element.css('background-color');
+    expect(backgroundColor).to.equal('rgb(255, 255, 255)'); // Use the RGB format directly
   });
+});
 
 
 
